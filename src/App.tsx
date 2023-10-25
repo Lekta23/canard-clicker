6ccda121d6a2ab586710783bbca9dfadd34b4e9e
TimotheeRiou <timo.riou@gmail.com>
2023-10-25 14:27:54 +0200

    add click effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./assets/Canard.png";
 import wallpaper from "./assets/Background.png";
 import "./App.css";
 import ButtonConnexion from "./composents/button_connexion";
 
-import { useState, useEffect } from "react";
 function App() {
-  const [haveMetamask, sethaveMetamask] = useState(true);
-  const [accountAddress, setAccountAddress] = useState("");
-  const [isConnected, setIsConnected] = useState(false);
+  const [isLogoEnlarged, setIsLogoEnlarged] = useState(false);
 
-  const { ethereum } = window;
+  const toggleLogoSize = () => {
+    setIsLogoEnlarged(true); // Activer l'agrandissement
 
-  const connectWallet = async () => {
-    try {
-      if (!ethereum) {
-        sethaveMetamask(false);
-      }
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      setAccountAddress(accounts[0]);
-      setIsConnected(true);
-    } catch (error) {
-      setIsConnected(false);
-    }
+    // Rétablir la taille du logo après quelques millisecondes (par exemple, 500 ms)
+    setTimeout(() => {
+      setIsLogoEnlarged(false);
+    }, 500);
   };
 
   const backgroundStyle = {
     backgroundImage: `url(${wallpaper})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    height: "100vh", // Set height to cover the entire viewport
+    height: "100vh",
+  };
+
+  const logoStyle = {
+    width: isLogoEnlarged ? "177px" : "187px", // Largeur du logo agrandi ou non
+    height: isLogoEnlarged ? "280px" : "290px", // Hauteur du logo agrandi ou non
+    cursor: "pointer",
+    transition: "all 0.2s", // Animation de transition pour un agrandissement instantané
   };
 
   return (
     <div style={backgroundStyle}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} style={logoStyle} alt="logo" onClick={toggleLogoSize} />
         <h1 className="text-3xl font-bold shadow-2xl">
           Clique sur le canard !
         </h1>
         <ButtonConnexion
           label="Connexion test"
           className="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded absolute top-0 right-0 m-4"
-          onClick={connectWallet}
         />
       </header>
     </div>
