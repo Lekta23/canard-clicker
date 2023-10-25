46d477881341e3e02685f53b216e5b2d87bfb6a5
TimotheeRiou <timo.riou@gmail.com>
2023-10-25 10:07:51 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1 className="text-3xl font-bold underline">
+          Ta gueule, c'est moi qui parle
+        </h1>
       </header>
     </div>
   );
