f1e6fb5ffe6a8571b65a89869ed98124cb88c706
Corentyn <48148602+Corentyn@users.noreply.github.com>
2023-10-25 12:52:55 +0200

    wallet connexion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from "react";
-
 import logo from "./assets/Canard.png";
 import wallpaper from "./assets/Background.png";
 import "./App.css";
 import ButtonConnexion from "./composents/button_connexion";
+
+import { useState, useEffect } from "react";
 function App() {
+  const [haveMetamask, sethaveMetamask] = useState(true);
+  const [accountAddress, setAccountAddress] = useState("");
+  const [isConnected, setIsConnected] = useState(false);
+
+  const { ethereum } = window;
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        sethaveMetamask(false);
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccountAddress(accounts[0]);
+      setIsConnected(true);
+    } catch (error) {
+      setIsConnected(false);
+    }
+  };
+
   const backgroundStyle = {
     backgroundImage: `url(${wallpaper})`,
     backgroundSize: "cover",
@@ -22,6 +44,7 @@ function App() {
         <ButtonConnexion
           label="Connexion test"
           className="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded absolute top-0 right-0 m-4"
+          onClick={connectWallet}
         />
       </header>
     </div>
